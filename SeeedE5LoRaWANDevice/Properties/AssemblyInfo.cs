46d1c37bcc8f456d4ab41a80e66793e802974840
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-05-28 17:53:38 +1200

    Updated (C) notice and versioning information as first "real" release

--- SeeedE5LoRaWANDevice/Properties/AssemblyInfo.cs
+++ SeeedE5LoRaWANDevice/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharp.BlankApplication")]
+[assembly: AssemblyTitle("SeeedE5LoRaWANDevice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharp.BlankApplication")]
-[assembly: AssemblyCopyright("Copyright © ")]
+[assembly: AssemblyCompany("devMobile Software")]
+[assembly: AssemblyProduct("devMobile Software LoRaWAN Library for Seeedstudio LoRa-E5")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 /////////////////////////////////////////////////////////////////
 // This attribute is mandatory when building Interop libraries //
